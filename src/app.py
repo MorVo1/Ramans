ce9df2bca071a558691b6ac1449e5a588eea5cc9
Maciej <maciejkuchta5@gmail.com>
2022-07-19 18:56:22 +0200

    .

--- src/app.py
+++ src/app.py
@@ -5,13 +5,14 @@ import tkinter.colorchooser
 import main
 
 root = Tk()
-root.geometry("800x564")
+root.geometry("1000x754")
 root.title('Ramans')
+root.state('zoomed')
+root.resizable(0, 0)
 
 # resizing background
-
-img = (Image.open("background.png"))
-resized = img.resize((2000, 1000), Image.Resampling.LANCZOS)
+img = Image.open("background.png")
+resized = img.resize((2000, 1200), Image.Resampling.LANCZOS)
 resized.save("background.png")
 
 # Define the PhotoImage Constructor by passing the image file
@@ -22,16 +23,18 @@ img_label = Label(root, image=img, bg='grey')
 #  define the position of the image
 img_label.place(x=0, y=0)
 
+final_img_label = Label(root)
+final_img_label.place(x=600, y=150, width=500, height=500)
+
 
 def show_picture():
+
     # Opening photo
     main.generate_raman()
     photo = PhotoImage(file='out.png')
 
-    # Placing photo in root
     final_img_label = Label(root, image=photo)
-
-    final_img_label.place(y=400, x=900)
+    final_img_label.place(x=600, y=150)
 
 
 # Buttons
@@ -40,6 +43,6 @@ button_generate = Button(root, text="GENERATE", pady=50, padx=80,
 # Window maximized
 root.state('zoomed')
 
-button_generate.place(x=200, y=400)
+button_generate.place(x=200, y=300)
 
 root.mainloop()
